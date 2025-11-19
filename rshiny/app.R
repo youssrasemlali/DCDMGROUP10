d55cf49c964845519303b95cc60b29db4ade2039
Sarah Lam <148975195+Slam0my@users.noreply.github.com>
2025-11-19 23:40:26 +0000

    Update app.R

--- rshiny/app.R
+++ rshiny/app.R
@@ -1,6 +1,23 @@
 #MAIN DASHBOARD
 
 
+########SQL CONNECT######
+#reads a mysqltable into rshiny app
+#DCDM_SQL<- dbConnect(RMySQL::MySQL(), 
+                     #username = "my_username", 
+                     #password = "my_password", 
+                     #host     = "host_address", 
+                     #port     = 3306,
+                     #dbname   = 'IMPC_phenotype_db')
+
+#load data
+#df <- dbGetQuery(DCDM_SQL, 'table_name') #reads as a data.frame
+
+
+
+
+######shinydashboard#######
+
 #library 
 library(shiny)
 library(dplyr)
@@ -26,7 +43,7 @@ ui <- dashboardPage(
   dashboardSidebar(
     sidebarMenu(
       menuItem("Phenotypic Scores for KO Gene", tabName = "phenoKOgene", icon =icon("dashboard")),
-      menuItem("All Mice Scores for a Phenotype", tabName = "2 Scores of All Mice for a Selected Phenotype", icon =icon("dashboard")),
+      menuItem("All Mice Scores for a Phenotype", tabName = "micescores", icon =icon("dashboard")),
       menuItem("Gene Clusters", tabName = "3 Gene Clusters", icon =icon("dashboard"))
     )
   ),
@@ -45,7 +62,18 @@ ui <- dashboardPage(
                 tableOutput("sigtable"),
               #our first plot 
                 plotlyOutput("p1_gene", height ="750px")
-        )
+        ),
+      
+      tabItem(tabName = "micescores", #to link to the menuItem
+              h2("All Mice Scores for a Phenotype"), #header
+              #dropdown for ko gene selection
+              selectInput("phenotype",
+                          "Select the Specfic Phenotype:",
+                          choices = sort(unique(IMPC_data$parameter_name))),
+              #our significant table 
+              tableOutput("sigtable2"),
+              #our second plot 
+              plotlyOutput("p2_mouse", height ="750px"))
       )
     )
 )
@@ -56,20 +84,20 @@ server <- function(input, output) {
   # 1- Phenotypic Scores for KO Gene
   #reactive - reruns the code based on input (gene KO chosen)
   genedf <- reactive({
-    df <- IMPC_data %>% filter(gene_symbol == input$gene)
+    df1 <- IMPC_data %>% filter(gene_symbol == input$gene)
   })
   
   #generating an interactive scatter plot 
   output$p1_gene <- renderPlotly({
-    df <- genedf() #gets the dataframe from the input
+    df1 <- genedf() #gets the dataframe from the input
     
     #defines the significant pvalues
-    df$significant <- factor(df$pvalue < 0.05,
+    df1$significant <- factor(df1$pvalue < 0.05,
                              levels = c(FALSE,TRUE),
                              labels = c("Not Significant","Significant"))
     
     #plotting the graph 
-    plot_ly(data = df, 
+    plot_ly(data = df1, 
             x = ~parameter_name,
             y = ~logpvalue,
             type = "scatter", #scatter plot
@@ -104,6 +132,93 @@ server <- function(input, output) {
   
   # create our significance table
   output$sigtable <- renderTable({
+    df1 <- genedf() #retrieves the selected KO gene
+    df1 %>%
+      filter(pvalue < 0.05) %>% #only keeps significant values
+      select(parameter_name, pvalue) %>% #our table headers
+      arrange(pvalue) %>% #sort based on most signficant at the top 
+      mutate(pvalue = sprintf("%.10f", pvalue)) #format pvalue to 10dp
+  },
+  caption = "Significant Phenotypes (P-values (10dp) < 0.05)",
+  caption.placement = "top",
+  align = 'lr')#left to right: parameter_name then pvalue order in columns
+  
+  #2 All Mice Scores for a Phenotype
+  #reactive - reruns the code based on input (parameterchosen)
+  phenodf <- reactive({
+    df2 <- IMPC_data %>% filter(parameter_name == input$phenotype)
+  })
+  
+  #generating an interactive scatter plot 
+  output$p2_mouse <- renderPlotly({
+    df2 <- phenodf() #gets the dataframe from the input
+    
+    
+    df2$significant <- factor(df2$pvalue < 0.05,
+                              levels = c(FALSE,TRUE),
+                              labels = c("Not Significant","Significant"))
+    
+    plot_ly(data = df2, 
+            x = ~gene_symbol,
+            y = ~logpvalue,
+            type = "scatter", #scatter plot
+            mode = "markers", #datapoints as markers
+            marker = list(size = 10),
+            color = ~significant, #2 colours for Not/Signficant
+            #hover- what it shows when user hovers over a point
+            text =~paste0(
+              "Gene: ", gene_symbol,"<br>",
+              "P-value: ", sprintf("%.10f", pvalue)), #%.10f defines 10dp, sprintf combines text and variable format
+            #text to be inserted into hover
+            hoverinfo = "text"
+    ) %>%
+      layout(
+        title = paste("All P-Value Scores for the Phenotype:", input$phenotype),
+        yaxis = list(title = "-log10(P-value)"),
+        xaxis = list(title = "KO Gene"),
+        #shapes inserts our red dashed line to see the significance threshold
+        shapes = list(
+          list(type = "line",
+               y0 = -log10(0.05), #axis 
+               y1 = -log10(0.05),
+               x0 = 0, 
+               x1 = 1,
+               yref = "y", 
+               xref = "paper",
+               line = list(color = "red", dash = "dash"))
+        )
+      )
+  })
+  # create our significance table
+  output$sigtable2 <- renderTable({
+    df2 <- phenodf() #retrieves the selected KO gene
+    df2 %>%
+      filter(pvalue < 0.05) %>% #only keeps significant values
+      select(gene_symbol,parameter_name, pvalue) %>% #our table headers
+      arrange(pvalue) %>% #sort based on most signficant at the top 
+      mutate(pvalue = sprintf("%.10f", pvalue)) #format pvalue to 10dp
+  },
+  caption = "Significant KO Genes(P-values (10dp) < 0.05)",
+  caption.placement = "top",
+  align = 'l')#left to right
+  
+  
+  
+  
+  
+  }
+#end of server
+
+#launch Shiny application 
+shinyApp(ui = ui, server = server)
+
+               xref = "paper",# makes the line span across whole x axis/plot
+               line = list(color = "red", dash = "dash"))
+      )
+  })
+  
+  # create our significance table
+  output$sigtable <- renderTable({
     df <- genedf() #retrieves the selected KO gene
     df %>%
       filter(pvalue < 0.05) %>% #only keeps significant values
@@ -120,3 +235,4 @@ server <- function(input, output) {
 
 #launch Shiny application 
 shinyApp(ui = ui, server = server)
+
